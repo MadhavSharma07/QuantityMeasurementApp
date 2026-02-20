1ae033627b211db9c76002ff13a84ae9d5e886de
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2026-02-20 13:51:30 +0530

    [Madhav Sharma]Add . Measurement Quantity App UC -3

--- QuantityMeasurementApp/src/quantitymeasurement/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/src/quantitymeasurement/QuantityMeasurementApp.java
@@ -5,117 +5,38 @@ import java.util.Scanner;
 
 public class QuantityMeasurementApp {
 
- // feet class
-    public static class Feet {
-
-        private final double value;
-
-        public Feet(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-
-            if (this == obj)
-                return true;
-
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-
-            Feet other = (Feet) obj;
-
-            return Double.compare(this.value, other.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
-    }
-
-  // inches class
-    public static class Inches {
-
-        private final double value;
-
-        public Inches(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-
-            if (this == obj)
-                return true;
-
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-
-            Inches other = (Inches) obj;
-
-            return Double.compare(this.value, other.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
-    }
-
-   // comparison methods
-
-    public static boolean compareFeet(double value1, double value2) {
-        return new Feet(value1).equals(new Feet(value2));
-    }
-
-    public static boolean compareInches(double value1, double value2) {
-        return new Inches(value1).equals(new Inches(value2));
-    }
-
-  // main method
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         try {
-
-            System.out.println("Select Unit:");
-            System.out.println("1. Feet");
-            System.out.println("2. Inches");
-
-            int choice = Integer.parseInt(scanner.nextLine());
-
-            System.out.print("Enter first value: ");
+            System.out.println("Enter first value:");
             double value1 = Double.parseDouble(scanner.nextLine());
 
-            System.out.print("Enter second value: ");
+            System.out.println("Enter first unit (FEET / INCHES):");
+            Length.LengthUnit unit1 =
+                    Length.LengthUnit.valueOf(scanner.nextLine().toUpperCase());
+
+            System.out.println("Enter second value:");
             double value2 = Double.parseDouble(scanner.nextLine());
 
-            boolean result = false;
+            System.out.println("Enter second unit (FEET / INCHES):");
+            Length.LengthUnit unit2 =
+                    Length.LengthUnit.valueOf(scanner.nextLine().toUpperCase());
 
-            switch (choice) {
-                case 1:
-                    result = compareFeet(value1, value2);
-                    break;
+            Length length1 = new Length(value1, unit1);
+            Length length2 = new Length(value2, unit2);
 
-                case 2:
-                    result = compareInches(value1, value2);
-                    break;
-
-                default:
-                    System.out.println("Invalid choice!");
-                    return;
-            }
+            boolean result = length1.equals(length2);
 
             System.out.println("Equal: " + result);
 
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input! Please enter numeric values only.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid input or unsupported unit.");
         }
 
         scanner.close();
     }
 }
+
 
